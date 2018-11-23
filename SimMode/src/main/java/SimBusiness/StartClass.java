056eb866d3ea0b12c117fd41ddab9be2a82f4cd3
unknown <tcz@tongcangzhen.xin>
2018-11-23 14:31:54 +0800

    1123

--- SimMode/src/main/java/SimBusiness/StartClass.java
+++ SimMode/src/main/java/SimBusiness/StartClass.java
@@ -1,20 +1,45 @@
 package SimBusiness;
 
+import com.sun.org.apache.xerces.internal.parsers.IntegratedParserConfiguration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Properties;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.*;
 
 public class StartClass {
+    static int threadNum;
+    private static void init() {
+        if(threadNum==0) {
+            Properties props = new Properties();
+            File f = null;
+            String path = System.getProperty("user.dir") + f.separator + "config.properties";
+            // ResourceBundle resourceBundle = ResourceBundle.getBundle("config");
+            try {
+                props.load(new FileInputStream(path));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            threadNum= Integer.valueOf(props.getProperty("threadNum"));
+        }
+    }
     public static void main(String[] args) throws InterruptedException {
+        init();
         BlockingQueue<WorkData> queue = new LinkedBlockingDeque<WorkData>();
         Set<Integer> set = new HashSet<Integer>();
         WorkProducer p1 = new WorkProducer(queue,set);
-        WorkConsumer c1 = new WorkConsumer(queue,set);
-        WorkConsumer c2 = new WorkConsumer(queue, set);
         ExecutorService service = Executors.newCachedThreadPool();
+        for (int i = 0; i < threadNum; i++) {
+            WorkConsumer workConsumer = new WorkConsumer(queue, set);
+            service.execute(workConsumer);
+        }
         service.execute(p1);
-        service.execute(c1);
-        service.execute(c2);
+
+
 //        Thread.sleep(10*1000);
 //        p1.stop();
 //        Thread.sleep(3000);
